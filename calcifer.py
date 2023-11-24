55e083f2e2d76a7637363c08f9b8146730d1dac6
Philip Cander <>
2023-11-24 13:30:25 +0100

    OOP is the way to go

--- calcifer.py
+++ calcifer.py
@@ -2,45 +2,70 @@ import sys
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QLineEdit
 from PyQt6.QtGui import QPalette, QColor
 
-# Create an instance of QApplication
-# Create a window
-# Set the window title
-# Set the geometry of the window (x, y, width, height)
-app = QApplication([])
-window = QWidget()
-window.setWindowTitle("Calcifer")
-window.setGeometry(100, 100, 320, 420)
 
-# Create a palette
-# Set the color for the Window role to a custom color
-# Apply the palette to the window
-palette = QPalette()
-palette.setColor(QPalette.ColorRole.Window, QColor("#002b36"))
-window.setPalette(palette)
+# Create a class that inherits from QWidget
+class Init_QWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+    def event(self, event):
+        return super().event(event)
 
-# Create a label for the Output
-# Move the label to a specific position
-# Set the color and font size of the label
-output_lbl = QLabel("<h1>Output</h1>", parent=window)
-output_lbl.move(20, 80)
-output_lbl.setStyleSheet("color: #93a1a1; font-size: 20px;")
-# Create a label for the History
-# Move the label to a specific position
-# Set the color and font size of the label
-history_lbl = QLabel("<h1>History</h1>", parent=window)
-history_lbl.move(20, 130)
-history_lbl.setStyleSheet("color: #93a1a1; font-size: 12px;")
-# Create an input box
-# Move the input box to a specific position
-# Set the color and font size of the input box and remove the border
-inputBox = QLineEdit(window)
-inputBox.setGeometry(20, 10, 160, 100)
-inputBox.setStyleSheet("border: none; background-color: #002b36; color: #93a1a1; font-size: 20px;")
 
-# Show the window
-# Exit the application when the window is closed
-window.show()
-sys.exit(app.exec())
+               
+class Calcifer():
+    def __init__(self):
+        # Create an instance of QApplication
+        # Create a window
+        # Set the window title
+        # Set the geometry of the window (x, y, width, height)
+        self.app = QApplication([])
+        self.window = Init_QWidget()
+        self.window.setWindowTitle("Calcifer")
+        self.window.setGeometry(100, 100, 320, 420)
+
+        # Create a palette
+        # Set the color for the Window role to a custom color
+        # Apply the palette to the window
+        self.palette = QPalette()
+        self.palette.setColor(QPalette.ColorRole.Window, QColor("#002b36"))
+        self.window.setPalette(self.palette)
+
+        # Call the init_lbl() 
+        # and init_input() methods
+        # Show the window
+        # Exit the application upon closing the window
+        self.init_lbl()
+        self.init_input()
+        self.window.show()
+        sys.exit(self.app.exec())
+
+    def init_lbl(self):
+        # Create a label for the Output
+        # Move the label to a specific position
+        # Set the color and font size of the label
+        self.output_lbl = QLabel("<h1>0</h1>", parent=self.window)
+        self.output_lbl.move(20, 80)
+        self.output_lbl.setStyleSheet("color: #93a1a1; font-size: 20px;")
+        # Create a label for the History
+        # Move the label to a specific position
+        # Set the color and font size of the label
+        self.history_lbl = QLabel("<h1>History</h1>", parent=self.window)
+        self.history_lbl.move(20, 130)
+        self.history_lbl.setStyleSheet("color: #93a1a1; font-size: 12px;")
+
+    def init_input(self):
+        # Create an input box
+        # Move the input box to a specific position
+        # Set the color and font size of the input box and remove the border
+        self.inputBox = QLineEdit(self.window)
+        self.inputBox.setGeometry(20, 10, 160, 60)
+        self.inputBox.setStyleSheet("border: none; background-color: #002b36; color: #93a1a1; font-size: 20px;")
+
+
+
+
+# Create an instance of the Calcifer class
+calcifer = Calcifer()    
 
 #002b36 
 # primary 1
