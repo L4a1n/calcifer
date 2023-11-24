74614c207acc3c697937885fc887c12931cdd07e
Philip Cander <>
2023-11-24 15:09:40 +0100

    added calculation function

--- calcifer.py
+++ calcifer.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QLineEdit
-from PyQt6.QtGui import QPalette, QColor
+from PyQt6.QtGui import QPalette, QColor, QFontMetrics, QGuiApplication
+import math
 
 
 # Create a class that inherits from QWidget
@@ -9,7 +10,6 @@ class Init_QWidget(QWidget):
         super().__init__()
     def event(self, event):
         return super().event(event)
-
 
                
 class Calcifer():
@@ -34,8 +34,8 @@ class Calcifer():
         # and init_input() methods
         # Show the window
         # Exit the application upon closing the window
-        self.init_lbl()
         self.init_input()
+        self.init_lbl()
         self.window.show()
         sys.exit(self.app.exec())
 
@@ -43,25 +43,45 @@ class Calcifer():
         # Create a label for the Output
         # Move the label to a specific position
         # Set the color and font size of the label
-        self.output_lbl = QLabel("<h1>0</h1>", parent=self.window)
+        self.output_lbl = QLabel("<h1> 0 </h1>", parent=self.window)
         self.output_lbl.move(20, 80)
         self.output_lbl.setStyleSheet("color: #93a1a1; font-size: 20px;")
         # Create a label for the History
         # Move the label to a specific position
         # Set the color and font size of the label
-        self.history_lbl = QLabel("<h1>History</h1>", parent=self.window)
+        self.history_lbl = QLabel("<h1> Output History... </h1>", parent=self.window)
         self.history_lbl.move(20, 130)
         self.history_lbl.setStyleSheet("color: #93a1a1; font-size: 12px;")
+
+    def calculate_and_update(self, text):
+        # Calculate the result of the expression
+        try:
+            result = str(eval(text, {"__builtins__": None}, {"math": math}))
+        except Exception as e:
+            if text == "":
+                result = "0"
+            else:
+                result = text
+    
+        # Update the text of the output label
+        self.output_lbl.setText("<h1>" + result + "</h1>")
+    
+        # Calculate the width of the text
+        font_metrics = QFontMetrics(self.output_lbl.font())
+        text_width = font_metrics.horizontalAdvance(result)
+    
+        # Set the width of the label
+        self.output_lbl.setFixedWidth(text_width*3)
 
     def init_input(self):
         # Create an input box
         # Move the input box to a specific position
         # Set the color and font size of the input box and remove the border
+        # Connect the input box to the update_output_lbl() method
         self.inputBox = QLineEdit(self.window)
         self.inputBox.setGeometry(20, 10, 160, 60)
         self.inputBox.setStyleSheet("border: none; background-color: #002b36; color: #93a1a1; font-size: 20px;")
-
-
+        self.inputBox.textChanged.connect(self.calculate_and_update)
 
 
 # Create an instance of the Calcifer class
